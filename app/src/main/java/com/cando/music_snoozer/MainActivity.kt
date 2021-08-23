1b8e3f9c6a65e7b2a0e5a202e142b10ec638dccc
Jeff Lee <jhlsuper@gmail.com>
2021-08-24 01:58:54 +0900

    onClick override + 알림창 demo / cutomView만들어야됨

--- app/src/main/java/com/cando/music_snoozer/MainActivity.kt
+++ app/src/main/java/com/cando/music_snoozer/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.cando.music_snoozer
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.media.AudioAttributes
 import android.media.AudioFocusRequest
 import android.media.AudioManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -13,19 +16,23 @@ import android.view.View
 import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.NotificationCompat
 import com.example.music_snoozer.R
 import com.google.android.gms.ads.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var state: State = State.BEFORE_PRESSING
     private var currentCountDownTimer: CountDownTimer? = null
 
     private var minToLong: Long = 0
     private var hourToLong: Long = 0
     private var realLong: Long = 0
-    private lateinit var mAdView :AdView
+    private lateinit var mAdView: AdView
+
+
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +40,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //광고 초기화
-        MobileAds.initialize(this){}
+        MobileAds.initialize(this) {}
         mAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
-
         val hour: NumberPicker = findViewById(R.id.numberPicker_hour)
         val min: NumberPicker = findViewById(R.id.numberPicker_min)
+
+
         hour.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
         min.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
 
@@ -49,52 +57,21 @@ class MainActivity : AppCompatActivity() {
         hour.maxValue = 6
         min.maxValue = 59
 
-        mAdView.adListener =object :AdListener(){
+        mAdView.adListener = object : AdListener() {
             override fun onAdLoaded() {
                 super.onAdLoaded()
+                Log.d("ads", "ad loaded")
 //                Toast.makeText(applicationContext, "ad Loaded", Toast.LENGTH_SHORT).show()
             }
 
             override fun onAdFailedToLoad(p0: LoadAdError) {
                 super.onAdFailedToLoad(p0)
-                Log.d("ads","$p0")
+                Log.d("ads", "$p0")
 
             }
         }
 
-        val timeSetView = findViewById<ConstraintLayout>(R.id.layout_setTime)
-        val countDownView = findViewById<ConstraintLayout>(R.id.layout_showRemainTime)
-        btn_start.setOnClickListener {
-            minToLong = (min.value * 1000 * 60).toLong()
-            hourToLong = (hour.value * 60 * 1000 * 60).toLong()
-            realLong = minToLong + hourToLong
-            when (state) {
-                State.BEFORE_PRESSING -> {
-                    
-
-                    currentCountDownTimer = createCountDownTimer(realLong)
-                    btn_start.text = "중지"
-                    timeSetView.visibility = View.INVISIBLE
-                    countDownView.visibility = View.VISIBLE
-                    state = State.ON_PRESSED
-                    Log.d("realLong", "$realLong")
-                    currentCountDownTimer?.start()
-//                    stopInSec(realLong)
-                }
-                State.ON_PRESSED -> {
-                    currentCountDownTimer?.cancel()
-                    btn_start.text = "시작하기"
-                    state = State.BEFORE_PRESSING
-                    timeSetView.visibility = View.VISIBLE
-                    countDownView.visibility = View.INVISIBLE
-                }
-
-
-            }
-//            Toast.makeText(this, "${hour.value}시간 ${min.value}분", Toast.LENGTH_SHORT).show()
-
-        }
-
+        btn_start.setOnClickListener(this)
     }
 
 
@@ -149,6 +126,68 @@ class MainActivity : AppCompatActivity() {
         txt_remainSeconds.text = "%02d초".format(remainSeconds % 60)
         txt_remainHours.text = "%02d시간".format(remainSeconds / 3600)
         txt_remainMinutes.text = "%02d분".format((remainSeconds / 60) % 60)
+    }
+
+    override fun onClick(v: View?) {
+        val hour: NumberPicker = findViewById(R.id.numberPicker_hour)
+        val min: NumberPicker = findViewById(R.id.numberPicker_min)
+
+        val timeSetView = findViewById<ConstraintLayout>(R.id.layout_setTime)
+        val countDownView = findViewById<ConstraintLayout>(R.id.layout_showRemainTime)
+
+        var builder = NotificationCompat.Builder(this, "MY_channel")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("미디어 종료 시각")
+            .setContentText("oh..")
+        val channelId = "MY_channel"
+        val channelName = "음악멈춰!"
+        val descriptionText = "descriptionText어쩌고쩌저고"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, channelName, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        
+        when (v?.id) {
+            R.id.btn_start -> {
+                minToLong = (min.value * 1000 * 60).toLong()
+                hourToLong = (hour.value * 60 * 1000 * 60).toLong()
+                realLong = minToLong + hourToLong
+
+
+
+                when (state) {
+                    State.BEFORE_PRESSING -> {
+
+
+                        currentCountDownTimer = createCountDownTimer(realLong)
+                        btn_start.text = "중지"
+                        timeSetView.visibility = View.INVISIBLE
+                        countDownView.visibility = View.VISIBLE
+                        state = State.ON_PRESSED
+                        Log.d("realLong", "$realLong")
+                        currentCountDownTimer?.start()
+
+                        notificationManager.createNotificationChannel(channel)
+
+                        notificationManager.notify(1000, builder.build())
+
+                    }
+                    State.ON_PRESSED -> {
+                        currentCountDownTimer?.cancel()
+                        btn_start.text = "시작하기"
+                        state = State.BEFORE_PRESSING
+                        timeSetView.visibility = View.VISIBLE
+                        countDownView.visibility = View.INVISIBLE
+                    }
+
+
+                }
+//            Toast.makeText(this, "${hour.value}시간 ${min.value}분", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
 
